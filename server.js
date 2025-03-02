fd70edbc9a4b33efa1fcdcd5d6721aa4bbdf1808
Akaash2004 <akaashraj.26it@licet.ac.in>
2025-03-02 21:14:38 +0530

    server.js

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-// db.js
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://127.0.0.1:27017/auctionDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-module.exports = mongoose;
-
-
-
 // server.js
 const express = require('express');
 const cors = require('cors');
